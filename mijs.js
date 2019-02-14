4a32929d4fa5120f3aa79512a9238f59fe543724
Nacho Garcia Cano <igarcia@beta08.aulas.gsyc.urjc.es>
2019-02-14 10:23:36 +0100

    comenzando la practica 2

---
+++ mijs.js
@@ -0,0 +1,10 @@
+function main(){
+  console.log("Que comiencen los juegos de JavaScript");
+
+  var boton = document.getElementById('boton')
+
+  boton.onclick = () => {
+    console.log("Click")
+  }
+
+}
