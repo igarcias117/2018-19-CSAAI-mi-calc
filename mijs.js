233b44564b6fd1bfaec15c8f53947f580c9fda4e
Nacho Garcia Cano <igarcia@beta07.aulas.gsyc.urjc.es>
2019-02-14 11:50:44 +0100

    prueba completada

--- mijs.js
+++ mijs.js
@@ -1,12 +1,12 @@
 function main() {
   console.log("Que comiencen los juegos de JavaScript");
 
-  var boton = document.getElementById('boton')
-
+  boton = document.getElementById('boton')
   boton.onclick = () => {
-    console.log("Click")
+    console.log("clickk")
 
-    var display = document.getElementById('display')
-    display.innerHTML = "NUEVO TEXTO"
+    display = document.getElementById('display')
+    display.innerHTML= "LOS ZUMITOS"
   }
+
 }
