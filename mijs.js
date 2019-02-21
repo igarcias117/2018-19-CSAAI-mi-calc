a0e6b3be7f3d824dc593fd2276cfd251a03adc3f
Nacho Garcia Cano <igarcia@beta07.aulas.gsyc.urjc.es>
2019-02-21 11:33:20 +0100

    empezando calculadora

--- mijs.js
+++ mijs.js
@@ -1,12 +1,28 @@
 function main() {
-  console.log("Que comiencen los juegos de JavaScript");
+  console.log("main();");
 
-  boton = document.getElementById('boton')
-  boton.onclick = () => {
-    console.log("clickk")
-
-    display = document.getElementById('display')
-    display.innerHTML = "Texto Nuevo"
+  var calc = {
+    gui: {
+      boton1 : document.getElementById('boton1'),
+      boton2 : document.getElementById('boton2'),
+      boton3 : document.getElementById('boton3'),
+      boton4 : document.getElementById('boton4'),
+      boton5 : document.getElementById('boton5'),
+      boton6 : document.getElementById('boton6'),
+      boton7 : document.getElementById('boton7'),
+      boton8 : document.getElementById('boton8'),
+      boton9 : document.getElementById('boton9'),
+      botons : document.getElementById('botons'),
+      botonm :  document.getElementById('botonm'),
+      botonx : document.getElementById('botonx'),
+      botond : document.getElementById('botond'),
+      igual: document.getElementById('igual'),
+      display: document.getElementById('display')
+    }
   }
-
+  contador = 0;
+  calc.gui.botons.onclick = () => {
+    contador += 1
+    calc.gui.display.innerHTML = contador
+  }
 }
