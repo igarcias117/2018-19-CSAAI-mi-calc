1381a410258102ba30741314b76499887018a3e0
Nacho Garcia Cano <igarcia@beta07.aulas.gsyc.urjc.es>
2019-02-28 12:31:52 +0100

    continuamos la calculadora

--- mijs.js
+++ mijs.js
@@ -3,6 +3,7 @@ function main() {
 
   var calc = {
     gui: {
+      boton0 : document.getElementById('boton0'),
       boton1 : document.getElementById('boton1'),
       boton2 : document.getElementById('boton2'),
       boton3 : document.getElementById('boton3'),
@@ -16,13 +17,49 @@ function main() {
       botonm :  document.getElementById('botonm'),
       botonx : document.getElementById('botonx'),
       botond : document.getElementById('botond'),
-      igual: document.getElementById('igual'),
-      display: document.getElementById('display')
+      igual : document.getElementById('igual'),
+      display : document.getElementById('display')
     }
   }
-  contador = 0;
-  calc.gui.botons.onclick = () => {
-    contador += 1
-    calc.gui.display.innerHTML = contador
+
+  calc.gui.boton1.onclick = () => {
+    calc.gui.display.innerHTML += "1";
   }
+
+  calc.gui.boton2.onclick = () => {
+    calc.gui.display.innerHTML += "2";
+  }
+
+  calc.gui.boton3.onclick = () => {
+    calc.gui.display.innerHTML += "3";
+  }
+
+  calc.gui.boton4.onclick = () => {
+    calc.gui.display.innerHTML += "4";
+  }
+
+  calc.gui.boton5.onclick = () => {
+    calc.gui.display.innerHTML += "5";
+  }
+
+  calc.gui.boton6.onclick = () => {
+    calc.gui.display.innerHTML += "6";
+  }
+
+  calc.gui.boton7.onclick = () => {
+    calc.gui.display.innerHTML += "7";
+  }
+
+  calc.gui.boton8.onclick = () => {
+    calc.gui.display.innerHTML += "8";
+  }
+
+  calc.gui.boton9.onclick = () => {
+    calc.gui.display.innerHTML += "9";
+  }
+
+  calc.gui.boton0.onclick = () => {
+    calc.gui.display.innerHTML += "0";
+  }
+
 }
